51b794d5886931d45032ecd6f40ca206de842e7e
Nithya S <23083@omnicell.com>
2023-08-21 13:08:26 +0530

    Product service helps to fetch json data

---
+++ src/app/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductService {
+    constructor(private http: HttpClient) { }
+
+    getJSONData(): Observable<any> {
+        return this.http.get('assets/products.json');
+    }
+}
