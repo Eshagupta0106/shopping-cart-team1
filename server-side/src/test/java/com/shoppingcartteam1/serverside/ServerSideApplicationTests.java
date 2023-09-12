7bb7912d63947c5ef16463a38f2e84dd059822b9
Palguni H <23149@omnicell.com>
2023-09-12 10:21:18 +0530

    Connected to mongodb atlas, added collection structures for users and products

---
+++ server-side/src/test/java/com/shoppingcartteam1/serverside/ServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcartteam1.serverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
